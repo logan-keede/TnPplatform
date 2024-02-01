430a34f9d8a03d807e2d1afeeb2c52c9cabc7120
Akash Dholakiya <akashdholakiya222@gmail.com>
2024-02-02 03:41:44 +0530

    form ui

--- TnPplatform/views.py
+++ TnPplatform/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import os
 from inspect import getsourcefile
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 @login_required
 def index(request):
-    if request == "POST":
-        pass    
+    if request.method == "POST":
+        print(request.body)
+
     
     return render(request, "Resume_generator.html")
