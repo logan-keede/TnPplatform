079514b479c414358f135ddf9c3f0f4a5dd1e8bf
logan-keede <68557630+logan-keede@users.noreply.github.com>
2024-02-20 15:57:44 +0530

    Fix migrations

--- Job_Opening/admin.py
+++ Job_Opening/admin.py
@@ -6,13 +6,3 @@ class JobAdmin(admin.ModelAdmin):
 
 admin.site.register(Job_Opening,JobAdmin)
 
-# Register your models here.
-# admin.py
-from django.contrib import admin
-from .models import Job_Opening 
-
-
-class JobAdmin(admin.ModelAdmin):
-    list_display = ('NameofCompany', 'profileOfCompany', 'JobProfile', 'BranchChoice', 'ctc', 'Eligibility', 'Selection', 'location', 'stipend', 'start')
-
-admin.site.register(Job_Opening, JobAdmin)
